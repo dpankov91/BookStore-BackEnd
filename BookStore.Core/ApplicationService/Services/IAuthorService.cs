d03130cfb75e0695a4afe08872dda2abe2b2c480
dpankov91 <dpankov91@gmail.com>
2020-12-22 21:17:32 +0200

    crud from service to repo done

--- BookStore.Core/ApplicationService/Services/IAuthorService.cs
+++ BookStore.Core/ApplicationService/Services/IAuthorService.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BookStore.Core.Entities;
 
 namespace BookStore.Core.ApplicationService.Services
 {
-    public class IAuthorService
+    public interface IAuthorService
     {
+        List<Author> ReadAllAuthors();
+
+        Author ReadAuthorById(int id);
+
+        Author CreateAuthor(Author author);
+
+        Author DeleteAuthor(int id);
+
+        Author UpdateAuthor(Author authorToUpdate);
     }
 }
