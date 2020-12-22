5d6e7331e5bd96ab009be242f7a8805720d96477
dpankov91 <dpankov91@gmail.com>
2020-12-22 22:33:16 +0200

    Put and delete methods done. Crud for author completed and tested in postman

--- BookStore-BackEnd/Controllers/AuthorController.cs
+++ BookStore-BackEnd/Controllers/AuthorController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Core.ApplicationService.Services;
+using BookStore.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,36 +14,97 @@ namespace BookStoreDbContext.Controllers
     [ApiController]
     public class AuthorController : ControllerBase
     {
+        private readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
         // GET: api/<AuthorController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<Author>> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                if(_authorService.ReadAllAuthors() != null)
+                {
+                    return Ok(_authorService.ReadAllAuthors());
+                }
+                return NotFound();
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
         }
 
         // GET api/<AuthorController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<Author> Get(int id)
         {
-            return "value";
+            try
+            {
+                if(id < 1)
+                {
+                    return BadRequest("Id must be bigger than 0");
+                }
+                return Ok(_authorService.ReadAuthorById(id));
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
         }
 
         // POST api/<AuthorController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<Author> Post([FromBody] Author author)
         {
+            try
+            {
+                return Ok(_authorService.CreateAuthor(author));
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
         }
 
         // PUT api/<AuthorController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<Author> Put(int id, [FromBody] Author author)
         {
+            try
+            {
+                if(id < 1 || author.Id != id)
+                {
+                    return BadRequest("Enter correct id. ID must be bigger than 1");
+                }
+                return _authorService.UpdateAuthor(author);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
         }
 
         // DELETE api/<AuthorController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<Author> Delete(int id)
         {
+            try
+            {
+                if(id < 1)
+                {
+                    return BadRequest("Enter correct id. ID must be bigger than 1");
+                }
+                _authorService.DeleteAuthor(id);
+                return Ok("Author with id:" + id + " successfully deleted");
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
         }
     }
 }
