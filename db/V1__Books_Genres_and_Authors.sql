9f7315a832592c9117de134f649b7408ccd14645
dpankov91 <dpankov91@gmail.com>
2021-05-17 18:10:05 +0300

    some files for db

---
+++ db/V1__Books_Genres_and_Authors.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Author(
+	Id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	Name VARCHAR(75) NOT NULL
+)
+
+CREATE TABLE Genre(
+	Id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	Name VARCHAR(75) NOT NULL
+)
+
+CREATE TABLE Book(
+	Id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	Name VARCHAR(75) NOT NULL,
+    PublishYear DateTime NOT NULL,
+    AuthorId int NOT NULL,
+    GenreId int NOT NULL
+
+    CONSTRAINT FK_Book_Author FOREIGN KEY (AuthorId)     
+    REFERENCES dbo.Author (AuthorId)     
+    ON DELETE NO ACTION    
+    ON UPDATE NO ACTION
+
+    CONSTRAINT FK_Book_Genre FOREIGN KEY (GenreId)     
+    REFERENCES dbo.Genre (GenreId)     
+    ON DELETE NO ACTION    
+    ON UPDATE NO ACTION
+)
